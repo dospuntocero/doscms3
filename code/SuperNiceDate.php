c44a630e0854fcbe15797cdcf1deed7fc589ef05
Francisco arenas <fa@dospuntocero.cl>
2012-09-26 17:47:34 -0700

    fechas ahora en español

--- code/SuperNiceDate.php
+++ code/SuperNiceDate.php
@@ -3,6 +3,7 @@ class SuperNiceDate extends DataExtension {
 	public function SuperNice() {
 		switch (i18n::get_locale()) {
 			case 'es_ES':
+			setlocale(LC_ALL, 'es_ES');
 			return $this->owner->FormatI18N("%d de %B de %Y");
 			case 'en_US':
 			return $this->owner->FormatI18N("%B %d, %Y");
