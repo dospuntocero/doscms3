b717e009e82a7316bbcce49b3d2f373db742000f
Francisco arenas <fa@dospuntocero.cl>
2012-11-22 08:54:00 -0800

    new translations

--- code/BaseConfig.php
+++ code/BaseConfig.php
@@ -21,28 +21,28 @@ class BaseConfig extends DataExtension {
 
   function updateCMSFields(FieldList $fields) {
     $fields->addFieldToTab('Root.Main', $gaCode = new TextField('GACode', 'Google Analytics account'));
-    $gaCode->setRightTitle('Account number to be used all across the site (in the format <strong>UA-XXXXX-X</strong>)');
+    $gaCode->setRightTitle(_t('BaseConfig.ACCOUNTNUMBER',"Account number to be used all across the site (in the format <strong>UA-XXXXX-X</strong>)"));
 
     $fields->addFieldToTab('Root.Main', $facebookURL = new TextField('FacebookURL', 'Facebook UID or username'));
-    $facebookURL->setRightTitle('Facebook link (everything after the "http://facebook.com/", eg http://facebook.com/<strong>username</strong> or http://facebook.com/<strong>pages/108510539573</strong>)');
+    $facebookURL->setRightTitle(_t('BaseConfig.FACEBOOKLINK','Facebook link (everything after the "http://facebook.com/", eg http://facebook.com/<strong>username</strong> or http://facebook.com/<strong>pages/108510539573</strong>)'));
 
     $fields->addFieldToTab('Root.Main', $twitterUsername = new TextField('TwitterUsername', 'Twitter username'));
-    $twitterUsername->setRightTitle('Twitter username (eg, http://twitter.com/<strong>username</strong>)');
+    $twitterUsername->setRightTitle(_t('BaseConfig.TWITTERUSER',"Twitter username (eg, http://twitter.com/<strong>username</strong>)"));
 
     $fields->addFieldToTab('Root.Main', $addThisID = new TextField('AddThisProfileID', 'AddThis Profile ID'));
-    $addThisID->setRightTitle('Profile ID to be used all across the site (in the format <strong>ra-XXXXXXXXXXXXXXXX</strong>)');
+    $addThisID->setRightTitle(_t('BaseConfig.ADDTHISPROFILE',"Profile ID to be used all across the site (in the format <strong>ra-XXXXXXXXXXXXXXXX</strong>)"));
 
-    $fields->addFieldToTab('Root.Logos', $logoField = new UploadField('Logo', 'Logo, to appear in the top left.'));
+    $fields->addFieldToTab('Root.Logos', $logoField = new UploadField('Logo', _t('BaseConfig.SITELOGO',"Logo, to appear in the top left.")));
     $logoField->getValidator()->setAllowedExtensions(array('jpg', 'jpeg', 'png', 'gif'));
     $logoField->setConfig('allowedMaxFileNumber', 1);
 
-    $fields->addFieldToTab('Root.Logos', $footerLogoField = new UploadField('FooterLogo', 'Footer logo, to appear in the bottom right.'));
+    $fields->addFieldToTab('Root.Logos', $footerLogoField = new UploadField('FooterLogo', _t('BaseConfig.FOOTERLOGO',"Footer logo, to appear in the bottom right.")));
     $footerLogoField->getValidator()->setAllowedExtensions(array('jpg', 'jpeg', 'png', 'gif'));
     $footerLogoField->setConfig('allowedMaxFileNumber', 1);
 
-    $fields->addFieldToTab('Root.Logos', $footerLink = new TextField('FooterLogoLink', 'Footer Logo link'));
-    $footerLink->setRightTitle('Please include the protocol (ie, http:// or https://) unless it is an internal link.');
+    $fields->addFieldToTab('Root.Logos', $footerLink = new TextField('FooterLogoLink', _t('BaseConfig.FOOTERLOGOLINK',"Footer Logo link")));
+    $footerLink->setRightTitle(_t('BaseConfig.FOOTERLOGOLINKINSTRUCTIONS',"Please include the protocol (ie, http:// or https://) unless it is an internal link."));
 
-    $fields->addFieldToTab('Root.Logos', new TextField('FooterLogoDescription', 'Footer Logo description'));
+    $fields->addFieldToTab('Root.Logos', new TextField('FooterLogoDescription', _t('BaseConfig.FOOTERLOGODESCRIPTION',"Footer Logo description")));
   }
 }
