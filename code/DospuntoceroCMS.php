c7005d7ddad8fbebca90d6d7d6d54b978130e3b2
francisco arenas <francisco.arenas@dospuntocero.cl>
2013-02-06 02:18:05 -0300

    no tables

--- code/DospuntoceroCMS.php
+++ code/DospuntoceroCMS.php
@@ -7,7 +7,7 @@ class DospuntoceroCMS extends LeftAndMainExtension{
 		$lines  = array('pastetext','ssmedia','separator','bold','italic','underline','strikethrough','hr','separator','styleselect','formatselect','separator','bullist','numlist','blockquote','sslink','unlink','anchor','separator','code');
 		$config = HtmlEditorConfig::get('cms');
 		$config->setButtonsForLine(1, $lines);
-		$config->setButtonsForLine(2, 'tablecontrols');
+		$config->setButtonsForLine(2, null);
 		$config->setButtonsForLine(3, null);
 
 		HtmlEditorConfig::get('cms')->setOption('theme_advanced_blockformats', 'p,h1,h2,h3,h4'); 
